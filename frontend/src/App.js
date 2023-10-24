bd822428f067beeef77d6b7b7445005e663d489c
RamkishoreBandla <bandlaramkishore090@gmail.com>
2023-10-24 00:50:01 -0400

    base

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <>
+    <div className="container">
+      <div className='row'>
+        <div className='col'>
+          <h1> Hello </h1>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
