6b0db99b2a3a232f7b7c132ab0ab6d32d15647e0
RamkishoreBandla <bandlaramkishore090@gmail.com>
2023-11-16 23:50:03 -0500

    ACoc alg

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,40 @@ const modelOps = require('../model/model');
 let router = express.Router();
 
 
-const ACoc = (inputs) => {
-  let output = [];
-  //write code
+const ACoc = (chars) => {
+  var numChars = chars.length;
+  var numTests = 1;
+  var temp = [];
 
-  return output;
+  for (var i = 0; i < numChars; i++) {
+    numTests *= chars[i].length;
+  }
+
+  var Ci = new Array(numChars).fill(0); // Initialize index variables for characteristics
+
+  for (var testNum = 1; testNum <= numTests; testNum++) {
+    //process.stdout.write("test # " + testNum + ": ");
+    var t1 = [];
+
+    for (var i = 0; i < numChars; i++) {
+      //process.stdout.write(chars[i][Ci[i]] + " ");
+      t1.push(chars[i][Ci[i]]);
+    }
+
+    temp.push(t1);
+    //process.stdout.write("\n");
+
+    for (var i = numChars - 1; i >= 0; i--) {
+      if (Ci[i] === chars[i].length - 1) {
+        Ci[i] = 0;
+      } else {
+        Ci[i]++;
+        break;
+      }
+    }
+  }
+  console.log(temp);
+  return temp;
 }
 
 const ECC = (inputs) => {
@@ -57,15 +86,15 @@ router.post('/getResults', async function (req, res, next) {
     //write API request to store result and input
     let storedData = await modelOps.storeInputandOutput(req.body, outputs);
 
-    outputs=[
-      ['A1','B1','C1'], ['A1','B1','C2'], ['A1','B2','C1'], ['A1','B2','C2'], ['A1','B3','C1'], ['A1','B3','C2'],
-['A2','B1','C1'], ['A2','B1','C2'], ['A2','B2','C1'], ['A2','B2','C2'], ['A2','B3','C1'], ['A2','B3','C2'],
-['A3','B1','C1'], ['A3','B1','C2'], ['A3','B2','C1'], ['A3','B2','C2'], ['A3','B3','C1'], ['A3','B3','C2']
+    outputs = outputs.length > 0 ? outputs : [
+      ['A1', 'B1', 'C1'], ['A1', 'B1', 'C2'], ['A1', 'B2', 'C1'], ['A1', 'B2', 'C2'], ['A1', 'B3', 'C1'], ['A1', 'B3', 'C2'],
+      ['A2', 'B1', 'C1'], ['A2', 'B1', 'C2'], ['A2', 'B2', 'C1'], ['A2', 'B2', 'C2'], ['A2', 'B3', 'C1'], ['A2', 'B3', 'C2'],
+      ['A3', 'B1', 'C1'], ['A3', 'B1', 'C2'], ['A3', 'B2', 'C1'], ['A3', 'B2', 'C2'], ['A3', 'B3', 'C1'], ['A3', 'B3', 'C2']
     ]
 
     res.json(outputs);
   } catch (error) {
-console.log(error);
+    console.log(error);
     res.statusCode = error.status || 500;
     res.json(error);
   }
